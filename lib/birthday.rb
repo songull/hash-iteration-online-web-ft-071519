e1451f3ef336ca3c9af42d9bdd69c99f2307245e
Kyler Barber <kylerbarber820@yahoo.com>
2019-07-28 19:54:05 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -10,11 +10,4 @@ birthday_kids = {
    end
  end
 
- winner = ""
- passengers.each do |suite, name|
-   if suite == :suite_a && name.start_with?("A")
-     winner = name
-   end
- end
-
- winner
+ 
